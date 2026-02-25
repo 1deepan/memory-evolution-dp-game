21c43336412c3ff607a8381b782631d1d8cca543
1deepan <dk190268@gmail.com>
2026-02-25 10:58:21 +0530

    FINAL PRO DESIGN - Interactive Dynamic Programming Visualizer UI added

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Game-based activity demonstrating Dynamic Programming using Memory Evolution concept. 
 ## ?? Project Demo Page 
 Open index.html to view simple DP simulation UI. 
+## ?? Interactive DP Visualizer Added 
+Open index.html and click Visualize DP to see animated table filling. 
--- dp.js
+++ dp.js
@@ -1,10 +1,24 @@
-function runDP(){ 
 let grid=[[2,4,1],[3,2,5],[4,3,2]]; 
+let container=document.getElementById("grid"); 
+function drawGrid(dp){ 
+container.innerHTML=""; 
+for(let i=0;i<dp.length;i++){ 
+for(let j=0;j<dp[0].length;j++){ 
+let div=document.createElement("div"); 
+div.className="cell"; 
+div.textContent=dp[i][j]===null?"":dp[i][j]; 
+container.appendChild(div); 
+}} 
+} 
+function sleep(ms){return new Promise(r=,ms));} 
+async function runDP(){ 
 let n=grid.length,m=grid[0].length; 
 let dp=Array.from({length:n},()=
 dp[0][0]=grid[0][0]; 
-for(let i=1;i<n;i++) dp[i][0]=dp[i-1][0]+grid[i][0]; 
-for(let j=1;j<m;j++) dp[0][j]=dp[0][j-1]+grid[0][j]; 
-for(let i=1;i<n;i++){for(let j=1;j<m;j++){dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);}} 
-document.getElementById("output").textContent=JSON.stringify(dp,null,2); 
+drawGrid(dp); 
+await sleep(500); 
+for(let i=1;i<n;i++){dp[i][0]=dp[i-1][0]+grid[i][0];drawGrid(dp);await sleep(500);} 
+for(let j=1;j<m;j++){dp[0][j]=dp[0][j-1]+grid[0][j];drawGrid(dp);await sleep(500);} 
+for(let i=1;i<n;i++){for(let j=1;j<m;j++){dp[i][j]=grid[i][j]+Math.min(dp[i-1][j],dp[i][j-1]);drawGrid(dp);await sleep(500);}} 
 } 
+drawGrid(Array.from({length:3},()=
